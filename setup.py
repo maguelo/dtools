354726263fa72217d035ff6a743dbe16dc1ebcc9
Miguel Angel Maldonado <maguelo@stark-2.local>
2021-03-22 19:14:14 +0100

    Initial version

---
+++ setup.py
@@ -0,0 +1,18 @@
+'''
+
+@author: Miguel Maldonado
+'''
+# !/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='dtools',
+      version='1.0',
+      description='Dataset tools',
+      author='Miguel Maldonado',
+      author_email='miguelangelmal[AT]gmail[DOT]com',
+      url='',
+      packages=['dtools'],
+      install_requires=[],
+      )
+
